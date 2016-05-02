baf33e4b0f9ba673401ca910a4c57e188fb67735
mvorobyov <mikhail.a.vorobyov@gmail.com>
2016-05-02 03:45:16 +0300

    fix typo

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('artists', function() {
-    this.route('show', { path: ':id' })
+    this.route('show', { path: ':id' });
   });
 });
 
